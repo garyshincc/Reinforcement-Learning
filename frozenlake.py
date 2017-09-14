b3b2f30538c6fb24cb4bef7d356aeb3ec8062481
Won Young (Gary) Shin <garyshincc@gmail.com>
2017-09-14 00:22:14 -0400

    updated frozen lake

--- frozenlake.py
+++ frozenlake.py
@@ -9,33 +9,32 @@ import tensorflow as tf
 class FrozenLake:
 	def __init__(self):
 		self.env = gym.make('FrozenLake-v0')
-		self.learning_rate = 0.1
+		self.learning_rate = 0.01
 		self.decay_params = 0.99
-		self.num_episodes = 1000
+		self.num_episodes = 5000
 		self.r_list = list()
-		self.exploration_rate = 0.7
-		self.exploration_decay = 0.99
-		self._init_model()
+		self.exploration_rate = 0.1
 		self.one_hot_identity_matrix = np.identity(16)
-
-	def _init_model(self):
-		tf.reset_default_graph()
-		# input one-hot vector of state
-		self.inputs1 = tf.placeholder(shape=[1,16], dtype=tf.float32)
-		self.weights = tf.Variable(tf.random_uniform([16,4]), dtype=tf.float32)
-		self.out_q = tf.matmul(self.inputs1, self.weights)
-		# get max value!
-		self.predict = tf.argmax(self.out_q, 1)
-
-		self.next_q = tf.placeholder(shape=[1,4], dtype=tf.float32)
-		loss = tf.reduce_sum(tf.square(self.next_q - self.out_q))
-		trainer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
-		self.update_model = trainer.minimize(loss)
 
 	def one_hot(self, state):
 		return self.one_hot_identity_matrix[state: state+1]
 
 	def start(self):
+		tf.reset_default_graph()
+		# input one-hot vector of state
+		self.inputs1 = tf.placeholder(shape=[1,16], dtype=tf.float32)
+		self.weights = tf.Variable(tf.random_uniform([16,4]), dtype=tf.float32)
+		self.out_q = tf.matmul(self.inputs1, self.weights)
+		#self.activated = self.out_q
+		self.activated = tf.nn.sigmoid(self.out_q)
+		# get max value!
+		self.predict = tf.argmax(self.activated, 1)
+
+		self.next_q = tf.placeholder(shape=[1,4], dtype=tf.float32)
+		self.loss = tf.reduce_sum(tf.square(self.next_q - self.activated))
+		self.trainer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+		self.update_model = self.trainer.minimize(self.loss)
+
 		init = tf.initialize_all_variables()
 		with tf.Session() as sess:
 			sess.run(init)
@@ -46,39 +45,38 @@ class FrozenLake:
 				done = False
 
 				for j in range(100):
-					if (np.random.rand(1) < self.exploration_rate):
-						action = self.env.action_space.sample()
-					else:
-						action, all_q = sess.run([self.predict, self.out_q], feed_dict={
+					action, all_q = sess.run([self.predict, self.activated], feed_dict={
 							self.inputs1: self.one_hot(prev_state)
 						})
-						action = action[0]
+					action = action[0]
+					if (np.random.rand(1) < self.exploration_rate):
+						action = self.env.action_space.sample()
+					
 					# act!
 					state, reward, done, info = self.env.step(action)
 					# get derivative values
-					next_q = sess.run([self.out_q], feed_dict={
+					next_q = sess.run([self.activated], feed_dict={
 						self.inputs1: self.one_hot(state)
 						})
 
 					# maximum likely q value is....!!!
 					max_q = np.max(next_q)
-					target_q = sess.run([self.out_q], feed_dict={
-							self.inputs1: self.one_hot(prev_state)
-						})[0]
+					target_q = all_q
+					#print (target_q)
 					target_q[0, action] = reward + self.decay_params * max_q
 
 					# train!
-					update_model, weights = sess.run([self.update_model, self.weights], feed_dict={
-						self.inputs1: self.one_hot(state),
+					_, __ = sess.run([self.update_model, self.weights], feed_dict={
+						self.inputs1: self.one_hot(prev_state),
 						self.next_q: target_q
 						})
-
+					#print (sess.run(self.weights))
 
 					net_reward += reward
 					prev_state = state
 					if done:
 						print ("Episode {}/{} complete.".format(i, self.num_episodes))
-						self.exploration_rate *= self.exploration_decay
+						self.exploration_rate = (1.0 / ((i / 20) + 10))
 						break
 				self.r_list.append(net_reward)
 
