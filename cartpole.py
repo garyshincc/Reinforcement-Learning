aaac58d2583ed814db1c64347f2c653585d0ed24
Gary Shin <garyshincc@gmail.com>
2017-09-16 13:18:40 -0400

    adjusted parameters

--- cartpole.py
+++ cartpole.py
@@ -20,8 +20,9 @@ class cartPoleModelAgent:
 		self.state_size = state_size
 		self.action_size = action_size
 		self.discount_rate = 0.95
-		self.learning_rate = 0.001
-		self.memory = collections.deque(maxlen=3000)
+		self.learning_rate = 0.01
+		self.learning_decay = 0.01
+		self.memory = collections.deque(maxlen=50000)
 
 	def load_model(self):
 		if os.path.isfile("cartPoleModel.json"):
@@ -53,14 +54,20 @@ class cartPoleModelAgent:
 		print ("Weights saved!")
 
 		print ("Saving full train version.")
+		trainX_batch = []
+		trainY_batch = []
 		for sample in self.memory:
 			state, action, reward, done, next_state = sample
 			target = reward
 			if (not done):
-				target = reward + self.discount_rate * np.amax(self.model.predict(np.reshape(next_state, [1,4]))[0])
+				target = reward + self.discount_rate * np.max(self.model.predict(np.reshape(next_state, [1,4]))[0])
 			train_target = self.model.predict(np.reshape(state, [1,4]))
 			train_target[0][action] = target
-			self.model.fit(np.reshape(state, [1,4]), train_target, epochs=1, verbose=0)
+			trainX_batch.append(np.reshape(state, [1,4])[0])
+			trainY_batch.append(train_target[0])
+			
+		self.model.fit(np.array(trainX_batch), np.array(trainY_batch), batch_size=len(trainX_batch), epochs=800)
+
 		model_json = self.model.to_json()
 		with open("cartPoleModelFull.json", "w") as open_file:
 			open_file.write(model_json)
@@ -70,11 +77,10 @@ class cartPoleModelAgent:
 
 	def create_model(self):
 		self.model = Sequential()
-		self.model.add(Dense(units=16, input_shape=(4,), activation='relu'))
-		self.model.add(Dense(units=16, activation='relu'))
-		self.model.add(Dense(units=16, activation='relu'))
+		self.model.add(Dense(units=24, input_shape=(4,), activation='tanh'))
+		self.model.add(Dense(units=48, activation='tanh'))
 		self.model.add(Dense(units=self.action_size, activation='linear'))
-		self.model.compile(optimizer=Adam(lr=self.learning_rate), loss='mse')
+		self.model.compile(optimizer=Adam(lr=self.learning_rate, decay=self.learning_decay), loss='mse')
 
 	def remember(self, observation):
 		self.memory.append(observation)
@@ -85,7 +91,9 @@ class cartPoleModelAgent:
 		except ValueError:
 			batch = random.sample(self.memory, len(self.memory))
 
-		print ("training a batch of: {} memory cells".format(len(batch)))
+		#print ("training a batch of: {} memory cells".format(len(batch)))
+		trainX_batch = []
+		trainY_batch = []
 		for sample in batch:
 			state, action, reward, done, next_state = sample
 			target = reward
@@ -93,12 +101,13 @@ class cartPoleModelAgent:
 			# goal is to increase the 'previous state' with
 			# best find of next state
 			if (not done):
-				target = reward + self.discount_rate * np.amax(self.model.predict(np.reshape(next_state, [1,4]))[0])
+				target = reward + self.discount_rate * np.max(self.model.predict(np.reshape(next_state, [1,4]))[0])
 			train_target = self.model.predict(np.reshape(state, [1,4]))
 			train_target[0][action] = target
 			#print ("train_target after: {}".format(train_target))
-
-			self.model.fit(np.reshape(state, [1,4]), train_target, epochs=1, verbose=0)
+			trainX_batch.append(np.reshape(state, [1,4])[0])
+			trainY_batch.append(train_target[0])
+		self.model.fit(np.array(trainX_batch), np.array(trainY_batch), batch_size=len(trainX_batch), epochs=1, verbose=0)
 
 	def action(self, state):
 		action = self.model.predict(np.reshape(state, [1,4]))
@@ -113,9 +122,9 @@ class CartPole:
 	def __init__(self):
 		self.env = gym.make('CartPole-v0')
 		self.agent = cartPoleModelAgent(self.env.observation_space.shape[0], self.env.action_space.n)
-		self.num_reinforce = 512
-		self.exploration_rate = 0.2
-		self.exploration_decay = 0.99
+		self.num_reinforce = 128
+		self.exploration_rate = 1
+		self.exploration_decay = 0.995
 		self.min_exploration_rate = 0.01
 
 	def load_model(self):
@@ -128,29 +137,29 @@ class CartPole:
 		self.agent.save_model()
 
 	def run_episode(self, num_episodes=250, num_frames=200):
-		print ("Running " + str(num_episodes) + " episodes, " + str(num_frames) + " frames each")
+		print ("Running {} episodes, {} frames each".format(num_episodes,num_frames))
 
 		for episode in range(num_episodes):
 			state = self.env.reset()
 
 			for frame in range(num_frames):
-				self.env.render()
+				#self.env.render()
 				action = self.agent.action(state)
 				if (np.random.rand() < self.exploration_rate):
 					action = self.env.action_space.sample()
 
 				observation, reward, done, info = self.env.step(action)
-				next_state = np.reshape(observation, [1,4])
 
-				status = (state, action, reward, done, next_state)
+				status = (state, action, reward, done, observation)
 				self.agent.remember(status)
 
-				state = next_state
+				state = observation
 				if (done):
 					print ("episode: {}/{}, score: {}".format(episode, num_episodes, frame))
-					self.exploration_rate *= self.exploration_decay
-					self.exploration_rate = max(self.exploration_rate, self.min_exploration_rate)
 					break
+
+			if (self.exploration_rate > self.min_exploration_rate):
+				self.exploration_rate *= self.exploration_decay
 			self.agent.reinforce(self.num_reinforce)
 
 	def play(self):
